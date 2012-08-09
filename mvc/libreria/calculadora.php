6cf43e99572e54e7432aebc5c396b588b536361a
colab_01.diem <runtim3.error@gmail.com>
2012-08-09 07:14:37 -0600

    ...

---
+++ mvc/libreria/calculadora.php
@@ -0,0 +1,21 @@
+<?php
+class ListaDeLineasX{
+	var $numeroDeConceptos;
+	var $descuentos;	
+	var $impuestos;
+	var subtotal;
+	var $total;
+	var $pago;
+	
+	function agregarConcepto(){
+		
+	}
+}
+
+class lineaX(){
+	var $cantidad;
+	var $UM;
+	var 
+
+}
+?>
