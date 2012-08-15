de3691885d9823c98a44d090dc232fff2b92860e
colab_01.diem <runtim3.error@gmail.com>
2012-08-14 22:47:27 -0600

    agrege iconos y requisitos para doctrex

---
+++ _modulos/doctrex/doctrex.php
@@ -0,0 +1,10 @@
+seleccionar un modelo
+asignar nombre al catalogo
+seleccionar icono
+seleccionar ubicacion en menu
+seleccionar formulario y/o lista paginada
+seleccionar los campos a mostrar, etiquetas, orden, componente extjs (checkbox, datefield, etc)
+seleccionar acciones para la barra de herramientas
+(seguridad peniente)
+
+crear *.xds y *.js, controlador y modelo.
