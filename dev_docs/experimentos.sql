0b4050b5b424548f05cfc591f238fc311be8e288
colab_01.diem <runtim3.error@gmail.com>
2012-09-28 22:27:30 -0600

    bdd

--- dev_docs/experimentos.sql
+++ dev_docs/experimentos.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50508
 File Encoding         : 65001
 
-Date: 2012-09-27 23:47:49
+Date: 2012-09-28 22:27:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -223,6 +223,28 @@ CREATE TABLE `fak_parte_de_concepto` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for `scrum_estados_de_historia`
+-- ----------------------------
+DROP TABLE IF EXISTS `scrum_estados_de_historia`;
+CREATE TABLE `scrum_estados_de_historia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` char(255) NOT NULL,
+  `icon` char(255) NOT NULL DEFAULT '',
+  `default` bit(1) NOT NULL DEFAULT b'0',
+  `descripcion` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of scrum_estados_de_historia
+-- ----------------------------
+INSERT INTO `scrum_estados_de_historia` VALUES ('1', 'Pendiente', '', '', '');
+INSERT INTO `scrum_estados_de_historia` VALUES ('2', 'En Proceso', '', '', '');
+INSERT INTO `scrum_estados_de_historia` VALUES ('3', 'Aprobar', '', '', '');
+INSERT INTO `scrum_estados_de_historia` VALUES ('4', 'Listo', '', '', '');
+INSERT INTO `scrum_estados_de_historia` VALUES ('5', 'En produccion', '', '', '');
+
+-- ----------------------------
 -- Table structure for `scrum_historias_de_usuario`
 -- ----------------------------
 DROP TABLE IF EXISTS `scrum_historias_de_usuario`;
@@ -234,40 +256,41 @@ CREATE TABLE `scrum_historias_de_usuario` (
   `es_backlog` tinyint(1) NOT NULL,
   `fk_sprint` int(11) DEFAULT NULL,
   `detalles` text,
+  `fk_estado` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of scrum_historias_de_usuario
 -- ----------------------------
-INSERT INTO `scrum_historias_de_usuario` VALUES ('1', 'ROOT', '0', '0', '1', null, null);
-INSERT INTO `scrum_historias_de_usuario` VALUES ('9', 'Backlog', '1', '3', '1', '0', 'asdf');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('11', 'Manual como el de Extjs4 o el mismo si es posible.', '1', '4', '1', '0', '<br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('17', 'DSA', '1', '3', '0', '9', null);
-INSERT INTO `scrum_historias_de_usuario` VALUES ('18', 'Mover historias de usuario entre sprints y backlog', '2', '1', '1', '0', 'Recargar el combo con la lista de sprints y el backlog en primer lugar, omitiendo la ubicacion actual');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('19', 'maxlenght:250mmaxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxaxlenght:250', '2', '0', '1', null, null);
-INSERT INTO `scrum_historias_de_usuario` VALUES ('24', 'Notificar al eliminar elementos, en comrportamiento grid y formulario', '4', '4', '0', '0', null);
-INSERT INTO `scrum_historias_de_usuario` VALUES ('26', 'Comportamiento Grid', '7', '4', '1', '0', '<br><ul><li>filtrar resultados</li><li><br></li></ul>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('27', 'Al cambiar de proyecto, refrescar el tab activo', '8', '1', '1', '0', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('28', 'General', '8', '1', '1', '0', '<br>crear combo Status<br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('29', 'quiero crear Historias de usuario y gestionarlas', '8', '1', '0', '1', '<span style=\"color: rgb(51, 153, 102);\">crear catalogo de historias de usuario.<br><br>quiero mover las historias entre backlog y sprints.<br></span><br>quiero que el listado de historias de usuario diga <br><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Backlog</b>:&nbsp; Historia X Ã³&nbsp; <br><span style=\"font-weight: bold;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; sprint X</span>: historia Y<br><br>quiero priorizar las historias, moviendolas.<br><br>agregar categorias (sin subcategorias) al formulario y agregar filtro al\n grid, permitir multiseleccion para cambiar prioridad por grupo<br><br>agregar campos de historia adicionales de manera configurable (Â¿por proyecto?)<br><ul><li>Componentes</li><li>Solicitante</li><li>Bug tracking ID<br></li></ul>dividir historia<br><br>quiero agregar estados a las historias y filtrar los resultados por esos estados, agregar explicacion para cada estado:<br><ul><li>terminada</li><li>lista</li><li>en espera</li><li>en proceso<br></li></ul>quiero que todos puedan crear historias, pero que solo el dueÃ±o de producto pueda agregarlas al backlog y al sprint.<br><br>quiero que las tareas notifiquen a los participantes del proyecto, de cambios (de estado principalmente), de creacion y de eliminacion.<br><br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('30', 'Comportamiento Formulario', '11', '4', '1', '0', 'Al eliminar, el formulario debe quedar limpio, y se debe preguntar si cerrar.<br><br>El formulario debe notificar de cambios pendientes antes de cerrar.<br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('35', 'quiero gestionar proyectos', '22', '1', '0', '1', '<br><span style=\"color: rgb(0, 128, 128);\">crear catalogo proyecto<br><br>agregar combo para seleccionar proyecto y que el label del combo abra el catalogo de proyectos, que se seleccione por default el primer proyecto en caso<br>de no existir en sesion.<br><br></span>Refrescar el tab activo al seleccionar proyecto<br><br>en caso de no existir un prroyecto seleccionado, todos los modulos lanzan un mensaje de error notificando de la situacion, <br><br>poder configurar un proyecto default<br><br>mostrar en el calendario, las fechas de los sprint s de diferentes proyectos<br><br><br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('36', 'que los usuarios puedan comentar acerca de las historias y tareas', '15', '1', '1', '0', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('37', 'Implementar roles de Scrum', '15', '1', '1', '0', '<br><font color=\"003366\" size=\"6\">Roles de Scrum.</font><br><br><b>DueÃ±o de producto:</b><br><br><b>Arbitro Scrum:</b><br><br><b>equipo de scrum:</b><br><br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('38', 'Implementar Reuniones de EstimaciÃ³n', '15', '1', '1', '0', '<br>â€‹permitir reuniones sincronas y asincronas<br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('39', 'quiero crear tareas, y gestionarlas', '15', '1', '1', '0', 'crear catalogo de tareas, <br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('40', 'Scrum', '15', '1', '1', '0', '<br>Scrum es un comportamiento, para los involucrados en la creaciÃ³n de software.<br><br>EstÃ¡ Enfocado a resultados rÃ¡pidos y periÃ³dicos. <br><br>Establece reglas generales y roles, como en los juegos deportivos.<br><br><br><br>Un Sprint es un periodo de trabajo, recomendado entre 20 y 40 dias (confirmar).<br><br>En ese periodo de trabajo, se realizan tareas por el equipo de desarrollo.<br><br>Idealmente las tareas han sido seleccionadas y estimadas pero&nbsp; pueden crearse al vuelo teniendo presente que en ese periodo debe producirse un entregable.<br><br>Se recomiendan equipos no mayores a 6 personas. <a href=\"http://google.com\"><span style=\"text-decoration: underline;\">leer mas</span></a>.<br>&nbsp;<br><br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('41', 'quiero crear Sprints y gestionarlos', '20', '1', '0', '1', '<br>agregar duracion del sprint<br><br>agregar fecha de inicio y fin<br><br>los sprints con fecha de inicio y fin se mostraran en el calendario<br><br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('42', 'Registrar las excepciones y el \"estack trace\"', '21', '2', '1', '0', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('44', 'priorizar las historias, moviendolas.', null, '1', '0', '1', '<br>â€‹mantener seleccionada la historia<br><br>al momento de guardar, establecer la prioridad mas baja para las historias de ese sprint<br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('45', 'SCRUM', '23', '1', '1', '0', '<br>Scrum es un comportamiento, para los involucrados en la creaciÃ³n de software.<br><br>EstÃ¡ Enfocado a resultados rÃ¡pidos y periÃ³dicos. <br><br>Establece reglas generales y roles, como en los juegos deportivos.<br><br><br><br>Un Sprint es un periodo de trabajo, recomendado entre 20 y 40 dias (confirmar).<br><br>En ese periodo de trabajo, se realizan tareas por el equipo de desarrollo.<br><br>Idealmente\n las tareas han sido seleccionadas y estimadas pero&nbsp; pueden crearse al \nvuelo teniendo presente que en ese periodo debe producirse un \nentregable.<br>');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('47', 'el controlador debe manejar excepciones generales y propias del sistema.', '0', '2', '1', '0', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('48', 'el controlador debe reconocer cuando responder con ajax y cuando con html', '0', '2', '1', '0', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('49', 'A', '1', '1', '0', '10', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('50', 'B', '3', '1', '0', '10', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('51', 'C', '4', '1', '0', '10', '');
-INSERT INTO `scrum_historias_de_usuario` VALUES ('52', 'D', '2', '1', '0', '10', '');
+INSERT INTO `scrum_historias_de_usuario` VALUES ('1', 'ROOT', '0', '0', '1', null, null, null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('9', 'Backlog', '1', '3', '1', '0', 'asdf', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('11', 'Manual como el de Extjs4 o el mismo si es posible.', '1', '4', '1', '0', '<br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('17', 'DSA', '1', '3', '0', '9', null, null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('18', 'Mover historias de usuario entre sprints y backlog', '0', '1', '0', '1', '<br><span style=\"background-color: rgb(0, 0, 255);\">Recargar el combo con la lista de sprints y el backlog en primer lugar, omitiendo la ubicacion actual</span>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('19', 'maxlenght:250mmaxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxlenght:250maxaxlenght:250', '2', '0', '1', null, null, null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('24', 'Notificar al eliminar elementos, en comrportamiento grid y formulario', '4', '4', '0', '0', null, null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('26', 'Comportamiento Grid', '7', '4', '0', '6', 'al eliminar con Ã©xito, mostrar top message<br><ul><li>filtrar resultados</li><li><br></li></ul>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('27', 'Al cambiar de proyecto, refrescar el tab activo', '-1', '1', '1', '0', '', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('28', 'General', '86', '1', '1', '0', '<br>crear combo Status<br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('29', 'quiero crear Historias de usuario y gestionarlas', '1', '1', '0', '1', '<span style=\"color: rgb(51, 153, 102);\">crear catalogo de historias de usuario.<br><br><br></span><br>quiero que el listado de historias de usuario diga <br><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Backlog</b>:&nbsp; Historia X Ã³&nbsp; <br><span style=\"font-weight: bold;\">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; sprint X</span>: historia Y<br><br>quiero priorizar las historias, moviendolas.<br><br>agregar categorias (sin subcategorias) al formulario y agregar filtro al\n grid, permitir multiseleccion para cambiar prioridad por grupo<br><br>agregar campos de historia adicionales de manera configurable (Â¿por proyecto?)<br><ul><li>Componentes</li><li>Solicitante</li><li>Bug tracking ID<br></li></ul>dividir historia<br><br>quiero agregar estados a las historias y filtrar los resultados por esos estados, agregar explicacion para cada estado:<br><ul><li>terminada</li><li>lista</li><li>en espera</li><li>en proceso<br></li></ul>quiero que todos puedan crear historias, pero que solo el dueÃ±o de producto pueda agregarlas al backlog y al sprint.<br><br>quiero que las tareas notifiquen a los participantes del proyecto, de cambios (de estado principalmente), de creacion y de eliminacion.<br><br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('30', 'Comportamiento Formulario', '11', '4', '0', '6', 'Al eliminar, el formulario debe quedar limpio, y se debe preguntar si cerrar.<br><br>El formulario debe notificar de cambios pendientes antes de cerrar.<br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('35', 'quiero gestionar varios proyectos', '4', '1', '0', '1', '<br style=\"color: rgb(0, 0, 255);\"><span style=\"color: rgb(0, 128, 128);\"><span style=\"color: rgb(0, 0, 255);\">crear catalogo proyecto</span><br style=\"color: rgb(0, 0, 255);\"><br style=\"color: rgb(0, 0, 255);\"><span style=\"color: rgb(0, 0, 255);\">agregar combo para seleccionar proyecto y que el label del combo abra el catalogo de proyectos, que se seleccione por default el primer proyecto en caso</span><br style=\"color: rgb(0, 0, 255);\"><span style=\"color: rgb(0, 0, 255);\">de no existir en sesion.</span><br><br></span><font color=\"FF0000\">en el grid, el boton eliminar no funciona<br><br>el combo no se selecciona por defaul, aparece un cero<br><br></font>Refrescar el tab activo al seleccionar proyecto<br><br>en caso de no existir un prroyecto seleccionado, todos los modulos lanzan un mensaje de error notificando de la situacion, <br><br>poder configurar un proyecto default<br><br>mostrar en el calendario, las fechas de los sprint s de diferentes proyectos<br><br>al cambiar de proyecto, cerrar todos los tabs del scrum, menos el backlog<br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('36', 'que los usuarios puedan comentar acerca de las historias y tareas', '87', '1', '1', '0', '', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('37', 'Implementar roles de Scrum', '88', '1', '1', '0', '<br><font color=\"003366\" size=\"6\">Roles de Scrum.</font><br><br><b>DueÃ±o de producto:</b><br><br><b>Arbitro Scrum:</b><br><br><b>equipo de scrum:</b><br><br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('38', 'Implementar Reuniones de EstimaciÃ³n', '99', '1', '1', '0', '<br>â€‹permitir reuniones sincronas y asincronas<br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('39', 'quiero crear tareas, y gestionarlas', '15', '1', '0', '1', 'crear catalogo de tareas, <br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('41', 'quiero crear Sprints y gestionarlos', '3', '1', '0', '1', '<br>agregar duracion del sprint<br><br>agregar fecha de inicio y fin<br><br>los sprints con fecha de inicio y fin se mostraran en el calendario<br><br><font color=\"FF0000\">en el catalogo no se borran los sprints</font><br><br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('42', 'Registrar las excepciones y el \"estack trace\"', '21', '2', '1', '0', '', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('44', 'quiero priorizar las historias.', '2', '1', '0', '1', '<br>En el formulario, permitir agregar una cantidad numÃ©rica, <br><ul><li>impedir repetir la importancia a las historias del mismo padre, <br></li><li>establecer una prioridad por default (la mas baja)</li></ul><br>En el grid:<br><ul style=\"background-color: rgb(255, 255, 255);\"><li><font color=\"0000FF\">moverlas con flechas up-down;</font></li></ul><ul><li>â€‹mantener seleccionada la historia, porque al moverla se selecciona siempre el primer elemento.</li></ul>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('45', 'SCRUM', '100', '1', '1', '0', '<br>Scrum es un comportamiento, para los involucrados en la creaciÃ³n de software.<br><br>EstÃ¡ Enfocado a resultados rÃ¡pidos y periÃ³dicos. <br><br>Establece reglas generales y roles, como en los juegos deportivos.<br><br><br><br>Un Sprint es un periodo de trabajo, recomendado entre 20 y 40 dias (confirmar).<br><br>En ese periodo de trabajo, se realizan tareas por el equipo de desarrollo.<br><br>Idealmente\n las tareas han sido seleccionadas y estimadas pero&nbsp; pueden crearse al \nvuelo teniendo presente que en ese periodo debe producirse un \nentregable.<br>', '2');
+INSERT INTO `scrum_historias_de_usuario` VALUES ('47', 'el controlador debe manejar excepciones generales y propias del sistema.', '0', '2', '1', '0', '', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('48', 'el controlador debe reconocer cuando responder con ajax y cuando con html', '0', '2', '1', '0', '', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('55', 'en el panel principal, quiero mostrar un resumen del estado de las historias, tanto el backlog como en los sprints.', '0', '1', '1', '0', 'Al nodo backlog y a cada nodo de sprint, agregar un subnodo por cada estado, indicando el numero de tareas que se encuentran en ese estado.<br><br>Al darle click a un nodo, abrir el grid de historias de usuario filtrando por ese status.<br>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('56', 'quiero agregar estados a las historias', '20', '1', '0', '1', 'form:<br><ul><li>Agregar combo estado en el toolbar, desabilitado en nuevo con estado por defaul seleccionado (desde configuracion)<br></li></ul><br>grid:<br><ul><li>agregar filtro por estados<br></li></ul><br><ul><li>crear catalogo estados, <span style=\"font-weight: bold;\">nombre </span>estado, <span style=\"font-weight: bold;\">icono </span>(caja de texto para escribir ruta),&nbsp; <span style=\"font-weight: bold;\">descripcion </span>(para mostrar como ayuda talvez en tooltip), <span style=\"font-weight: bold;\">default </span>(boton togle en el toolbar, cuando no hay uno default, seleccionar automaticamente, cuando ya existe default, no presionar)<br></li></ul><br>Grid Estado:<br><ul><li>Basico.<br></li></ul>Form Estado:<br><ul><li>configurar un estado por default.</li></ul><br><br>', '3');
+INSERT INTO `scrum_historias_de_usuario` VALUES ('57', 'Sprint 2', '0', '2', '0', '3', '', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('58', 'manejar modulos', '0', '2', '1', '0', '<br><br><font face=\"tahoma, arial, helvetica, sans-serif\"><span style=\"font-size: 12px;\">â€‹crear carpeta modulos, dentro de alli cada modulo con sus respectivos controladores, modelos y vistas.</span></font><br><font face=\"tahoma, arial, helvetica, sans-serif\"><span style=\"font-size: 12px;\">cada modulo tendria la estructura de la carpeta mvc, enconces las peticiones tendrian la forma:</span></font><br><br><b style=\"color: rgb(0, 0, 0); font-family: tahoma, arial, helvetica, sans-serif; font-size: 12px; \">MODULO/controlador/accion</b><br><br><ul><li><font face=\"tahoma, arial, helvetica, sans-serif\"><span style=\"font-size: 12px;\">crear pruebas unitarias</span></font></li></ul>', null);
+INSERT INTO `scrum_historias_de_usuario` VALUES ('59', 'crear pruenas unitarias', '30', '1', '1', '0', '', null);
 
 -- ----------------------------
 -- Table structure for `scrum_proyectos`
@@ -304,10 +327,10 @@ CREATE TABLE `scrum_sprint` (
 -- Records of scrum_sprint
 -- ----------------------------
 INSERT INTO `scrum_sprint` VALUES ('1', 'A Sprint 1', '1');
-INSERT INTO `scrum_sprint` VALUES ('3', 'B 1', '2');
-INSERT INTO `scrum_sprint` VALUES ('4', 'B 2', '2');
+INSERT INTO `scrum_sprint` VALUES ('3', 'Sprint  1', '2');
+INSERT INTO `scrum_sprint` VALUES ('4', 'Sprint 2', '2');
 INSERT INTO `scrum_sprint` VALUES ('5', 'C 1', '3');
-INSERT INTO `scrum_sprint` VALUES ('6', 'C2', '4');
+INSERT INTO `scrum_sprint` VALUES ('6', 'SPRINT 1', '4');
 INSERT INTO `scrum_sprint` VALUES ('8', 'dfa', '3');
 INSERT INTO `scrum_sprint` VALUES ('9', 'qqqqqqqqqqqq', '3');
 INSERT INTO `scrum_sprint` VALUES ('10', 'TESTS Priorizar', '1');
